bc98f69671eb89553b394f74c2c3d57d77e2fdee
MAC <mac@Foluwa.local>
2019-11-25 11:28:28 +0100

    25th Nov, I stopped at padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home()
-));
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: Text('My Ninja App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Hello Foluwa',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.blueGrey,
-              fontFamily: 'IndieFlower',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click Me'),
-          backgroundColor: Colors.green[600],
-        ),
-      );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Ninja App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text('MORONFOLUWA'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.green[600],
+      ),
+    );
   }
 }
-
